87ae2337f918dc80b46db3d49eb57753ec3cfcb5
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2024-10-13 17:49:41 +0530

    fixed the file uploading part with .env files

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,4 +1,4 @@
 import { OurFileRouter } from "@/app/api/uploadthing/core"
 import {generateReactHelpers} from "@uploadthing/react"
 
-export const {useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
+export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
