5b05759afedf49684d8efee2f03ff5e0783b1a5f
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2024-10-15 00:42:53 +0530

    adding the phone layout in STEP-2 for cover with custom colors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['utfs.io'],
+    },
+};
 
 export default nextConfig;
+// nextJs for some reason ask to make outerfiles to be a whitelist , we are putting "utfs.io"
+// the images from uploadthings as a part of whitelist
