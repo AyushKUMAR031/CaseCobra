c1a28180658989996515f2fcab7ec7f1bf0b8c77
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2024-11-08 13:06:41 +0530

    done with the order System -heading to payment

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -3,6 +3,7 @@
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/product"
 import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { Order } from "@prisma/client"
 
 export const createCheckoutSession = async ({configId,}:{configId : string}) => {
     const configuration = await db.configuration.findUnique({
@@ -31,4 +32,26 @@ export const createCheckoutSession = async ({configId,}:{configId : string}) => 
         price += PRODUCT_PRICES.material.polycarbonate
     }
 
+    let order: Order | undefined = undefined
+
+    const existingOrder = await db.order.findFirst({
+        where: {
+        userId: user.id,
+        configurationId: configuration.id,
+        },
+    })
+
+    console.log(user.id, configuration.id)
+
+    if (existingOrder) {
+        order = existingOrder
+    } else {
+        order = await db.order.create({
+          data: {
+            amount: price / 100,
+            userId: user.id,
+            configurationId: configuration.id,
+          },
+        })
+    }
 }
