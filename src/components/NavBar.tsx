4ecd553df024655868bfd3e5f7f877cc3e72f735
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2024-11-11 11:21:33 +0530

    Link fixing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getKindeServerSession, LoginLink, LogoutLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const NavBar = async () => {
     const { getUser } = getKindeServerSession()
@@ -18,14 +18,14 @@ const NavBar = async () => {
                     <div className="h-full flex items-center space-x-4">
                         {user ? (
                             <>
-                                <Link href="/api/auth/logout" className={buttonVariants({ size: 'sm', variant: "ghost" })}>Sign out</Link>
+                                <LogoutLink className={buttonVariants({ size: 'sm', variant: "ghost" })}>Sign out</LogoutLink>
                                 {isAdmin ? <Link href="/dashboard" className={buttonVariants({ size: 'sm', variant: "ghost" })}>Dashboard ✨</Link> : null}
                                 <Link href="/configure/upload" className={buttonVariants({ size: 'sm', className: "hidden sm:flex items-center gap-1", })}>Create case <ArrowRight className="ml-1.5 h-5 w-5" /></Link>
                             </>
                         ) : (
                             <>
-                                <Link href="/api/auth/register" className={buttonVariants({ size: 'sm', variant: "ghost" })}>Sign up</Link>
-                                <Link href="/api/auth/login" className={buttonVariants({ size: 'sm', variant: "ghost" })}>Login</Link>
+                                <RegisterLink className={buttonVariants({ size: 'sm', variant: "ghost" })}>Sign up</RegisterLink>
+                                <LoginLink className={buttonVariants({ size: 'sm', variant: "ghost" })}>Login</LoginLink>
                                 <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
                                 <Link href="/configure/upload" className={buttonVariants({ size: 'sm', className: "hidden sm:flex items-center gap-1", })}>Create case <ArrowRight className="ml-1.5 h-5 w-5" /></Link>
                             </>
